54469113152986db34970991b24dd5f8596f69e0
votienkhiem <votienkhiem@users.noreply.github.com>
2019-11-17 08:36:19 +0700

    giao diện

--- app/src/main/java/com/example/ailatrieuphu/Manageraccount.java
+++ app/src/main/java/com/example/ailatrieuphu/Manageraccount.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+public class Manageraccount extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_answerquestion);
+        setContentView(R.layout.activity_manageraccount);
     }
 }
