b9921d5bf9e7483dc30581bca210863776e1f037
Chanh Sau <dumo7894@gmail.com>
2019-12-27 12:00:25 +0700

    update-sap xep

--- app/src/main/java/com/example/ailatrieuphu/Rank.java
+++ app/src/main/java/com/example/ailatrieuphu/Rank.java
@@ -18,6 +18,8 @@ import org.json.JSONObject;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
 
 public class Rank extends AppCompatActivity {
 
@@ -41,10 +43,32 @@ public class Rank extends AppCompatActivity {
 //        int i=listnguoichoi.size();
 //        String s=String.valueOf(i);
 //        Toast.makeText(this,s,Toast.LENGTH_SHORT).show();
+        sapxep();
         recyclerView=findViewById(R.id.rcl_rank);
         adapter=new NguoiChoiAdapter(this,listnguoichoi);
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    }
+    public void sapxep(){
+        Collections.sort(listnguoichoi, new Comparator<NguoiChoi>() {
+            @Override
+            public int compare(NguoiChoi o1, NguoiChoi o2) {
+                int diem1,diem2;
+                diem1=Integer.parseInt(o1.diem_cao_nhat);
+                diem2=Integer.parseInt(o2.diem_cao_nhat);
+                if(diem1<diem2){
+                    return 1;
+                }
+                else{
+                    if(diem1==diem2){
+                        return 0;
+                    }
+                    else{
+                        return -1;
+                    }
+                }
+            }
+        });
     }
     public boolean getJson(String jsonString){
         try{
