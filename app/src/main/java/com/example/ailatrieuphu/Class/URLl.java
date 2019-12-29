508d2e56624ee115afd23510f5079a292f5aa920
Chanh Sau <dumo7894@gmail.com>
2019-12-29 11:42:14 +0700

    URL

--- app/src/main/java/com/example/ailatrieuphu/Class/URLl.java
+++ app/src/main/java/com/example/ailatrieuphu/Class/URLl.java
@@ -6,4 +6,7 @@ public class URLl {
     public static final String url_get_linh_vuc = base_url+"/api/linh-vuc";
     public static final String url_get_cau_hoi = base_url+"/api/cau-hoi?linh_vuc_id=";
     public static final String url_them_luot_choi = base_url+"/api/them-luot-choi";
+    public static final String url_xep_hang=base_url+"/api/bang-xep-hang";
+    public static final String url_goi_credit=base_url+"/api/goi-credit";
+ public static final String url_them_chi_tiet=base_url+"/api/them-chi-tiet";
 }
