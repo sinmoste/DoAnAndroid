f8f93106c91afb7524f83f0a5ca0944b91ac4d9c
votienkhiem <votienkhiem@users.noreply.github.com>
2020-01-01 20:14:04 +0700

    chỉnh sửa giao diện dialog hỗ trợ

--- app/src/main/java/com/example/ailatrieuphu/Class/URLl.java
+++ app/src/main/java/com/example/ailatrieuphu/Class/URLl.java
@@ -2,7 +2,7 @@ package com.example.ailatrieuphu.Class;
 
 public class URLl {
    // public static final String base_url = "http://192.168.153.2:8080/GameLaravel/public";
-    public static final String base_url ="http://10.0.2.2:8000";
+    public static final String base_url ="http://10.0.3.2:8000";
     public static final String url_get_linh_vuc = base_url+"/api/linh-vuc";
     public static final String url_get_cau_hoi = base_url+"/api/cau-hoi?linh_vuc_id=";
     public static final String url_them_luot_choi = base_url+"/api/them-luot-choi";
