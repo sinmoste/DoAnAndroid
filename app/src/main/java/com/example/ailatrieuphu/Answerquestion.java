c4151ba45ca99cd2e15a2f9034c7727a2da7eac7
Chanh Sau <dumo7894@gmail.com>
2020-01-02 22:08:57 +0700

    ???

--- app/src/main/java/com/example/ailatrieuphu/Answerquestion.java
+++ app/src/main/java/com/example/ailatrieuphu/Answerquestion.java
@@ -1,7 +1,5 @@
 package com.example.ailatrieuphu;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.content.Intent;
 import android.os.Bundle;
 import android.os.CountDownTimer;
@@ -10,6 +8,8 @@ import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.ailatrieuphu.Class.CauHoi;
 import com.example.ailatrieuphu.Class.ChiTietLuotChoi;
